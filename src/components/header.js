2cbd73aa80a39af90b901a5c2506509e69e5f45c
ruslanvs <ruslanvs@yahoo.com>
2018-07-19 14:53:06 -0700

    Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,36 @@
 // import libraries for making a componenet
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 // make a component
-const Header = () => {
-    const { textStyle } = styles;
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
 
-    return <Text style={textStyle}>Albums!</Text>;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
 };
+
 const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
     textStyle: {
-        fontSize: 50
+        fontSize: 25
     }
-}
+};
 
 // make the component available to other parts of the app
 export default Header;
