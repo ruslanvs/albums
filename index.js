35a3c413054fd32ada5c2ab3a108d545ea1c425c
ruslanvs <ruslanvs@yahoo.com>
2018-07-18 20:19:25 -0700

    Header with styling

--- index.js
+++ index.js
@@ -7,18 +7,15 @@
 // AppRegistry.registerComponent(appName, () => App);
 
 
-
-
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
-const App = () => {
-    return (
-        <Text>Some Text</Text>
-    );
-};
+const App = () => (
+    <Header />
+);
 
 // Render it to the device
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+// import libraries for making a componenet
+import React from 'react';
+import { Text } from 'react-native';
+
+
+// make a component
+const Header = () => {
+    const { textStyle } = styles;
+
+    return <Text style={textStyle}>Albums!</Text>;
+};
+const styles = {
+    textStyle: {
+        fontSize: 50
+    }
+}
+
+// make the component available to other parts of the app
+export default Header;
